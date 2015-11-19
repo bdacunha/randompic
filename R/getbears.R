465345b10073db045bfa391704b737acd0234240
Brenda DAcunha <bdacunha@outlook.com>
2015-11-19 11:51:51 -0800

    c

--- R/getbears.R
+++ R/getbears.R
@@ -1,11 +1,8 @@
 #' getbears
 #'
 #' @param x numeric
-#'
 #' @return character
 #' @export
-#'
-#' @examples
 getbears <- function(x){
 	stopifnot(is.numeric(x))
 	sprintf("![](http://placebear.com/g/%s/%s)", x, x)
