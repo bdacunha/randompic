8738f61b1b14c2bf9c24d2c46fd8930d184eb716
Brenda DAcunha <bdacunha@outlook.com>
2015-11-19 15:34:43 -0800

    commit

--- tests/testthat/test_getbacon.R
+++ tests/testthat/test_getbacon.R
@@ -5,3 +5,10 @@ test_that("the output is a character",
 x <- 400
 expect_is(getbacon(x), "character")
 })
+
+context("checking the input for getbacon")
+
+test_that("the input is numeric",
+{
+expect_error(getbacon("hola"))
+})
--- tests/testthat/test_getbears.R
+++ tests/testthat/test_getbears.R
@@ -5,3 +5,10 @@ test_that("the output is a character",
 	x <- 400
 expect_is(getbears(x), "character")
 })
+
+context("checking the input for getbears")
+
+test_that("the input is numeric",
+{
+expect_error(getbacon("rawr"))
+})
